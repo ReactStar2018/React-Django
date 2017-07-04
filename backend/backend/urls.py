5252690c9aaf868de9e8ffa14fd0825b69a21d51
WebStar27 <webstar.27@outlook.com>
2017-07-04 23:49:45 +0300

    React.js-Django
    Front-end : React.js
    Back-end : Python/Django

---
+++ backend/backend/urls.py
@@ -0,0 +1,36 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.contrib.auth.models import User
+from django.conf import settings
+from rest_framework import permissions, serializers, viewsets, routers
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'url', 'username', 'first_name', 'last_name', 'email', 'is_superuser', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAdminUser,)
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
+    url(r'^api/R/', include(router.urls)),
+    url(r'^api/F/', include('rest_framework.urls', namespace='rest_framework')),
+    url(r'^api/', include('backend.api.urls')),
+    url(r'^(?P<path>favicon\.ico)', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    url(r'^static/(?P<path>.*)', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+]
